072dca21bd1136ab29b6bfb7158cdc6ea86b8e76
Ajjig <majjig@pm.me>
2022-12-05 17:42:13 +0100

    Added port

--- parsing/server.cpp
+++ parsing/server.cpp
@@ -1,5 +1,6 @@
 #include "../include/header.hpp"
 
+
 Server::Server( std::vector<str> config ) {
 	parse(config);
 }
@@ -42,6 +43,18 @@ void Server::parse(std::vector<str> config) {
 			if (i + 1 >= config.size() || str("{:}").find(config[i + 1]) != std::string::npos) {
 				std::cout << "Error: root must be followed by a path" << std::endl;
 				exit(EXIT_FAILURE);
+			}
+		}
+		else if (config[i] == "port") {
+			if (i + 1 >= config.size() || str("{:}").find(config[i + 1]) != std::string::npos) {
+				std::cout << "Error: port must be followed by a number" << std::endl;
+				exit(EXIT_FAILURE);
+			} else {
+				_port = std::stoi(config[i + 1]);
+				if (_port < 0 || _port > 65535) {
+					std::cout << "Error: port must be 0 < p <= 65535" << std::endl;
+					exit(EXIT_FAILURE);
+				}
 			}
 		}
 	}
