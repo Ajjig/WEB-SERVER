63ae08ad7835a55c31ef5af5720c07252b6470c3
Ajjig <majjig@pm.me>
2022-12-05 16:49:32 +0100

    INIT PARSING

---
+++ parsing/server.cpp
@@ -0,0 +1,48 @@
+#include "../include/header.hpp"
+
+Server::Server( std::vector<str> config ) {
+	parse(config);
+}
+
+Server::~Server() {}
+
+void Server::addLocation(Location location) {
+	locations.push_back(location);
+}
+
+void Server::addIndex(str ind) {
+	_indexes.push_back(ind);
+}
+
+void Server::setRoot(str root) {
+	_root = root;
+}
+
+str Server::getRoot() {
+	return _root;
+}
+
+std::vector<str> Server::getIndexes() {
+	return _indexes;
+}
+
+std::vector<Location> Server::getLocations() {
+	return locations;
+}
+
+void Server::parse(std::vector<str> config) {
+
+	for (size_t i = 0; i < config.size(); i++) {
+		if (config[i] == "index")
+			while (i + 1 >= config.size() || str("{:}").find(config[i + 1]) != std::string::npos) {
+				addIndex(config[i + 1]);
+				i++;
+			}
+		else if (config[i] == "root") {
+			if (i + 1 >= config.size() || str("{:}").find(config[i + 1]) != std::string::npos) {
+				std::cout << "Error: root must be followed by a path" << std::endl;
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
