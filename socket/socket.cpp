f423a3f0eeacf1a06a208879b53776b007202c08
iidkhebb <iidkhebb@student.1337.ma>
2022-12-09 04:08:41 +0100

    added non blocking mode with epoll

--- socket/socket.cpp
+++ socket/socket.cpp
@@ -1,68 +1,174 @@
 #include "../include/socket.hpp"
 
-
-/**
- *  @brief Construct a new Socket:: Socket object
- *  socket(domain, type, protocol)
- *  domain: AF_INET (IPv4 protocol) or AF_INET6 (IPv6 protocol)
- *  type: SOCK_STREAM (TCP) or SOCK_DGRAM (UDP)
- *  protocol: 0 (IP)
- */
-Socket::Socket() : server_fd(0), new_socket(0), ready_descriptor(0), addrlen_cli(0), childpid(0)
+Socket::Socket()
 {
-    // Creating socket file descriptor
-    server_fd = socket(AF_INET, SOCK_STREAM, 0); // returns 0 is error
-    if (server_fd == 0)
-    {
-        perror("failed to create socket");
-        exit(EXIT_FAILURE);
+	master_socket = this->init_socket();
+	this->set_nonblocking(master_socket);
+	this->init_epoll();
+}
+
+Socket::~Socket()
+{
+	close(epfd);
+	close(master_socket);
+}
+
+int Socket::init_socket()
+{
+	if( (master_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {    
+        perror("sockfd\n");    
+        exit(1);    
     }
-    server_address.sin_family = AF_INET; // The address family we used when we set up the socket.
-    server_address.sin_addr.s_addr = htonl ( INADDR_ANY ); // machine’s IP address. htonl() converts the IP address to network byte order.
-    server_address.sin_port = htons( PORT ); // port number to bind to. htons() converts the port number to network byte order.
+
+	if( (master_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {    
+        perror("sockfd\n");    
+        exit(1);    
+    }    
+	bzero(&local, sizeof(local)); 
+    local.sin_family = AF_INET;    
+    local.sin_addr.s_addr = htonl(INADDR_ANY);;    
+    local.sin_port = htons(this->get_port());    
+
+	int optval = 1;
+	if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
+	{
+		perror("setsockopt");
+		exit(1);
+	}
+
+	if( bind(master_socket, (struct sockaddr *) &local, sizeof(local)) < 0) {    
+        perror("bind\n");    
+        exit(1);    
+    }    
+    listen(master_socket, MAX_FD);
+
+	return master_socket;
 }
 
-Socket::~Socket() {}
-
-/**
- * @brief Bind the socket to the address and port number specified in addr 
- * 
- * bind(server_fd, server_address, addrlen)
- *      - server_fd socket file descriptor
- *      - server_address server address
- *      - addrlen length of the address
- * 
- * The structure sockaddr is a generic container that 
- * just allows the OS to be able to read the first couple
- * of bytes that identify the address family. The address 
- * family determines what variant of the sockaddr struct to 
- * use that contains elements that make sense for that specific 
- * communication type. For IP networking, we use struct sockaddr_in, 
- * which is defined in the header netinet/in.h
- */
-void Socket::bind()
+void Socket::init_epoll()
 {
-    ::bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address));
+	epfd = epoll_create(MAX_EVENTS);    
+    if (epfd == -1) {    
+        perror("epoll_create");    
+        exit(EXIT_FAILURE);    
+    }      
+    ev.events = EPOLLIN;    
+    ev.data.fd = master_socket;    
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, master_socket, &ev) == -1) {    
+        perror("epoll_ctl: listen_sock");    
+        exit(EXIT_FAILURE);    
+    }
 }
 
-/**
- * @brief Listen for connections on a socket
- * listen(socket, backlog)
- * socket: socket file descriptor
- * backlog: maximum length to which the queue of pending connections for socket may grow
- * FD_ZERO: Initializes the file descriptor set fdset to have zero bits for all file descriptors.
- */
-void Socket::listen()
+int Socket::set_nonblocking(int sockfd)
 {
-    ::listen(server_fd, 10);
-    FD_ZERO(&readfds);
+	if(fcntl(sockfd, F_SETFL, O_NONBLOCK) < 0) {    
+        perror("fcntl(F_SETFL)\n");    
+        exit(1);    
+    }
 }
 
-/**
- * @brief Read file
- * @param filename
- * @return std::string 
- */
+void Socket::set_incoming_connection()
+{
+	while ((incoming_connection = accept(master_socket,(struct sockaddr *) &remote, &addrlen)) > 0) 
+	{    
+		set_nonblocking(incoming_connection); 
+		ev.events = EPOLLIN | EPOLLET;
+		ev.data.fd = incoming_connection;    
+		if (epoll_ctl(epfd, EPOLL_CTL_ADD, incoming_connection,&ev) == -1) 
+		{    
+			perror("epoll_ctl: add");    
+			exit(EXIT_FAILURE);    
+		}    
+	}    
+	if (incoming_connection == -1) 
+	{    
+		if (errno != EAGAIN && errno != ECONNABORTED && errno != EPROTO && errno != EINTR)     
+			perror("accept");    
+	}    
+}
+
+void Socket::read_fd()
+{
+	n = 0;    
+	while ((nread = read(fd, http_header + n, BUFSIZ-1)) > 0) 
+	{    
+		n += nread;    
+	}    
+	if (nread == -1 && errno != EAGAIN) 
+		perror("read error");
+
+	ev.data.fd = fd;    
+	ev.events = events[i].events | EPOLLOUT;    
+
+	if (epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev) == -1) 
+	{    
+		perror("epoll_ctl: mod");    
+	}  
+}
+
+void Socket::write_fd()
+{
+	std::string response = construct_response();
+	
+	sprintf(buffer, response.c_str(), response.length());    
+
+	int nwrite, data_size = strlen(buffer);    
+	n = data_size;    
+	while (n > 0) {    
+		nwrite = write(fd, buffer + data_size - n, n);    
+		if (nwrite < n) {    
+			if (nwrite == -1 && errno != EAGAIN) {    
+				perror("write error");    
+			}    
+			break;    
+		}    
+		n -= nwrite;    
+	}    
+	close(fd);    
+}
+
+void Socket::start()
+{
+	for (;;)
+	{
+		N_Files_discriptors = epoll_wait(epfd, events, MAX_EVENTS, -1); 
+		if (N_Files_discriptors == -1) 
+		{    
+			perror("epoll_pwait");    
+			exit(EXIT_FAILURE);    
+		}
+		for (i = 0; i < N_Files_discriptors; ++i)
+		{
+			fd = events[i].data.fd;    
+            if (fd == master_socket) 
+			{
+				set_incoming_connection();
+				continue;
+			}
+			if (events[i].events & EPOLLIN) 
+			{
+				read_fd();
+			}
+			if (events[i].events & EPOLLOUT)
+			{
+				write_fd();
+			}
+		}
+	}
+}
+
+int Socket::get_port()
+{
+	return PORT; // to be changed later
+}
+
+// Private methods
+std::string Socket::get_http_header()
+{
+	return std::string(http_header);
+}
+
 std::string Socket::read_file(char *filename)
 {
     std::ifstream inFile;
@@ -73,7 +179,8 @@ std::string Socket::read_file(char *filename)
         std::cout << "Error opening file" << std::endl;
         exit(1);
     }
-    // A stringstream associates a string object with a stream allowing you to read from the string as if it were a stream (like cin).
+    /* A stringstream associates a string object with a stream allowing you 
+	to read from the string as if it were a stream (like cin). */
     std::stringstream strStream;
     strStream << inFile.rdbuf(); //read the file
   
@@ -82,45 +189,11 @@ std::string Socket::read_file(char *filename)
 
 std::string Socket::construct_response()
 {
-    std::string out = read_file((char *)"./socket/index.html");
+    std::string out = read_file((char *)"../html/index.html");
     int file_size = out.length();
 
     std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " \
         + std::to_string(file_size) + "\n\n" + out;
 
     return response ;
-}
-
-void Socket::accept_socket()
-{
-    for (;;)
-    {
-        FD_SET(server_fd, &readfds);
-        ready_descriptor = select(server_fd + 1, &readfds, NULL, NULL, NULL);
-        
-        if (FD_ISSET(server_fd, &readfds))
-        {
-            addrlen_cli = sizeof(client_address);
-            new_socket = accept(server_fd, (struct sockaddr *)&client_address, &addrlen_cli);
-            if (!(childpid = fork()))
-            {
-                close(server_fd);
-                bzero(buffer, MAXLINES);
-                read( new_socket , buffer, sizeof(buffer));
-                std::cout << buffer << std::endl;
-                std::string response = construct_response();
-                write(new_socket, response.c_str(), response.length());
-                close(new_socket);
-                exit(0);
-            }
-            close(new_socket);
-        }
-    }
-}
-
-void Socket::start()
-{
-    bind();
-    listen();
-    accept_socket();
 }
