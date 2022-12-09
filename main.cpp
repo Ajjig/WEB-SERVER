15bbb1d2021af8f56b9d04ee53af0f8cd41ca972
Ajjig <majjig@pm.me>
2022-12-09 17:58:30 +0100

    uncomment

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main( int ac, char ** av ) {
 
 	Server server = parse(ac, av);
 	server.put(); // puts the server information
-	//Socket socket;
-	//socket.start();
+	Socket socket;
+	socket.start();
 	return 0;
 }
